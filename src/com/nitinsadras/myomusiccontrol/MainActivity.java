08cff156197f53b73e6a7d66b624919efdc04cd5
ugotnitined <nitin.sadras@gmail.com>
2014-10-04 02:25:10 -0700

    initial commit

---
+++ src/com/nitinsadras/myomusiccontrol/MainActivity.java
@@ -0,0 +1,115 @@
+package com.nitinsadras.myomusiccontrol;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.thalmic.myo.AbstractDeviceListener;
+import com.thalmic.myo.DeviceListener;
+import com.thalmic.myo.Hub;
+import com.thalmic.myo.Myo;
+import com.thalmic.myo.Pose;
+import com.thalmic.myo.scanner.ScanActivity;
+
+
+public class MainActivity extends Activity {
+
+	public Hub hub;
+	private DeviceListener mListener; 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        hub = Hub.getInstance();
+        if (!hub.init(this)) {
+            Log.e("test", "Could not initialize the Hub.");
+            finish();
+            return;
+        } 
+        
+        mListener = new AbstractDeviceListener() {
+    	    Context mContext = getApplicationContext();
+    		@Override
+    	    public void onConnect(Myo myo, long timestamp) {
+    	        Toast.makeText(mContext, "Myo Connected!", Toast.LENGTH_SHORT).show();
+    	    }
+
+    	    @Override
+    	    public void onDisconnect(Myo myo, long timestamp) {
+    	        Toast.makeText(mContext, "Myo Disconnected!", Toast.LENGTH_SHORT).show();
+    	    }
+
+    	    @Override
+    	    public void onPose(Myo myo, long timestamp, Pose pose) {
+    	        Log.d("pose", "Pose: " + pose);
+
+    	        //TODO: Do something awesome.
+    	    }
+    	};
+    	
+    	hub.addListener(mListener);
+        
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    
+    public void pauseMusic(View view){
+    	Context ctx = getApplicationContext();
+        AudioManager mAudioManager = (AudioManager) ctx.getSystemService(Context.AUDIO_SERVICE);    
+        if (mAudioManager.isMusicActive()) {
+            Intent mediaIntent = new Intent("com.android.music.musicservicecommand");
+            mediaIntent.putExtra("command", "pause");
+            ctx.sendBroadcast(mediaIntent);
+        }
+
+    }
+    
+    public void playMusic(View view){
+    	Context ctx = getApplicationContext();
+        AudioManager mAudioManager = (AudioManager) ctx.getSystemService(Context.AUDIO_SERVICE);    
+        Intent mediaIntent = new Intent("com.android.music.musicservicecommand");
+        mediaIntent.putExtra("command", "play");
+        sendBroadcast(mediaIntent);
+    }
+    
+    public void skip(View view){
+    	Context ctx = getApplicationContext();
+        AudioManager mAudioManager = (AudioManager) ctx.getSystemService(Context.AUDIO_SERVICE);    
+        Intent mediaIntent = new Intent("com.android.music.musicservicecommand");
+        mediaIntent.putExtra("command", "next");
+        sendBroadcast(mediaIntent);
+    }
+    
+    public void connect(View view){
+        Context context = getApplicationContext();
+        Intent intent = new Intent(context, ScanActivity.class);
+        startActivity(intent);
+        Log.d("test",  "done with scan activity");
+    }
+}
